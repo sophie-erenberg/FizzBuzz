61834e3486f3dd9227d179f185a9a51c09825aaf
Sophie <sophie.erenberg@gmail.com>
2024-09-09 20:28:53 -0400

    wrote main method for Reduce

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int steps = 0;
+        int n = 100;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else if (n % 2 != 0) {
+                n = n - 1;
+            }
+            steps++;
+        }
+        System.out.println(steps);
+    }
 }
